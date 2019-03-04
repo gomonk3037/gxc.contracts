63ef0ed47c981eb6a2c6aa6a6974190ee178ee9a
Jeeyong Um <conr2d@gmail.com>
2019-03-04 11:48:49 +0900

    Change a way to serialize `extended_symbol_code`

--- contracts/libraries/include/gxclib/symbol.hpp
+++ contracts/libraries/include/gxclib/symbol.hpp
@@ -4,21 +4,34 @@
  */
 #pragma once
 
+#include <eosiolib/symbol.hpp>
+
 namespace eosio {
 
    class extended_symbol_code {
    public:
-      constexpr extended_symbol_code(): value(0) {}
+      constexpr extended_symbol_code(): value(0)
+      {}
 
       constexpr explicit extended_symbol_code( uint128_t raw )
-      : value(raw) {}
+      : value(raw)
+      {}
 
-      extended_symbol_code( symbol_code s, name c ) {
-         value = static_cast<uint128_t>(s.raw()) << 64 | c.value;
-      }
+      extended_symbol_code( symbol_code s, name c )
+      : value( static_cast<uint128_t>(c.value) << 64 | s.raw() )
+      {}
 
       extended_symbol_code( symbol s, name c )
-      : extended_symbol_code( s.code(), c ) {}
+      : extended_symbol_code( s.code(), c )
+      {}
+
+      extended_symbol_code( extended_symbol es )
+      : extended_symbol_code( es.get_symbol(), es.get_contract() )
+      {}
+
+      extended_symbol_code( extended_asset ea )
+      : extended_symbol_code( ea.quantity.symbol, ea.contract )
+      {}
 
       constexpr uint128_t raw()const { return value; }
 
