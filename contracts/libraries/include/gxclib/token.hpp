d5fc9166ce10edc833311bacb544cda485e30187
Jeeyong Um <conr2d@gmail.com>
2019-03-21 16:29:51 +0900

    1.6.0 feature freeze

--- contracts/libraries/include/gxclib/token.hpp
+++ contracts/libraries/include/gxclib/token.hpp
@@ -33,7 +33,7 @@ asset get_balance(name owner, name issuer, symbol_code sym_code) {
    asset balance;
    auto esc = extended_symbol_code(sym_code, issuer);
    db_get_i64(db_find_i64(token_account.value, issuer.value, "accounts"_n.value,
-                          fasthash64(reinterpret_cast<const char*>(&esc), sizeof(uint128_t), "accounts"_n.value)),
+                          xxh3_64(reinterpret_cast<const char*>(&esc), sizeof(uint128_t))),
               reinterpret_cast<void*>(&balance), sizeof(asset));
    return balance;
 }
