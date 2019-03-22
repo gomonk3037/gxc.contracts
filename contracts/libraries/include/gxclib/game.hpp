1204d8f017ed3c94ad3de8d1ee6361dd23204021
Jeeyong Um <conr2d@gmail.com>
2019-03-23 01:12:56 +0900

    Assert if given account is not registered as game

--- contracts/libraries/include/gxclib/game.hpp
+++ contracts/libraries/include/gxclib/game.hpp
@@ -11,12 +11,13 @@
 namespace gxc {
 
 using eosio::name;
+using eosio::check;
 
 constexpr name game_account = "gxc.game"_n;
 
-inline bool check_is_game(name name) {
+void check_is_game(name name) {
    auto it = db_find_i64(game_account.value, game_account.value, "game"_n.value, basename(name).value);
-   return it != db_end_i64(game_account.value, game_account.value, "game"_n.value);
+   check(it != db_end_i64(game_account.value, game_account.value, "game"_n.value), "not registered to game account");
 }
 
 }
