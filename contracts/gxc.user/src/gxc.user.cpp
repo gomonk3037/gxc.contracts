7474a0d8deb94b04f7f087d8a46310c2c0149028
Jeeyong Um <conr2d@gmail.com>
2019-03-22 18:41:18 +0900

    Change the length limit of nickname

--- contracts/gxc.user/src/gxc.user.cpp
+++ contracts/gxc.user/src/gxc.user.cpp
@@ -4,9 +4,9 @@
  */
 #include <gxc.user/gxc.user.hpp>
 
-namespace gxc { namespace user {
+namespace gxc {
 
-void contract::authenticate(name account_name, name game_name, const string& login_token) {
+void user_contract::authenticate(name account_name, name game_name, const string& login_token) {
    check(login_token.size() == 16, "login_token has invalid length");
    require_auth(account_name);
 
@@ -14,25 +14,25 @@ void contract::authenticate(name account_name, name game_name, const string& log
    check(now() <= expire_time, "login_token is expired");
 }
 
-void contract::login(name account_name, name game_name, string login_token) {
+void user_contract::login(name account_name, name game_name, string login_token) {
    authenticate(account_name, game_name, login_token);
 }
 
-void contract::connect(name account_name, name game_name, string login_token) {
+void user_contract::connect(name account_name, name game_name, string login_token) {
    authenticate(account_name, game_name, login_token);
 }
 
-void contract::setnick(name account_name, string nickname) {
-   eosio_assert(nickname.size() <= 24, "nickname too long");
-   eosio_assert(is_valid_nickname(nickname), "invalid nickname");
+void user_contract::setnick(name account_name, string nickname) {
+   check(nickname.size() >= 6 && nickname.size() <= 24, "nickname has invalid length");
+   check(is_valid_nickname(nickname), "nickname contains invalid character");
 
-   eosio_assert(has_auth(system::account) || has_auth(account_name), "missing required authority");
+   check(has_auth(system::account) || has_auth(account_name), "missing required authority");
 
    nicktable nt(_self, _self.value);
 
    const auto& idx = nt.get_index<"nickname"_n>();
    auto dup = idx.find(sha256(nickname.data(), nickname.size()));
-   eosio_assert(dup == idx.end(), "nickname already occupied");
+   check(dup == idx.end(), "nickname already occupied");
 
    auto cb = [&](auto& n) {
       n.account_name = account_name;
@@ -43,15 +43,15 @@ void contract::setnick(name account_name, string nickname) {
 
    auto itr = nt.find(account_name.value);
    if (itr != nt.end()) {
-      eosio_assert(false, "nickname change not supported");
+      check(false, "nickname change not supported");
       //nt.modify(itr, same_payer, cb);
    } else {
       nt.emplace(payer, cb);
    }
 }
 
-void contract::rmvnick(name account_name) {
-   eosio_assert(false, "nickname removal not supported");
+void user_contract::rmvnick(name account_name) {
+   check(false, "nickname removal not supported");
 
    // control flow should not reach here
    require_auth(_self);
@@ -64,17 +64,17 @@ void contract::rmvnick(name account_name) {
    nt.erase(itr);
 }
 
-void contract::payram4nick(name account_name) {
+void user_contract::payram4nick(name account_name) {
    require_auth(account_name);
 
    nicktable nt(_self, _self.value);
 
    auto itr = nt.find(account_name.value);
-   eosio_assert(itr != nt.end(), "nickname is not set with given account");
+   check(itr != nt.end(), "nickname is not set with given account");
 
    nt.modify(itr, account_name, [&](auto& n) {});
 }
 
-} }
+}
 
-EOSIO_DISPATCH(gxc::user::contract, (login)(setnick)(payram4nick))
+EOSIO_DISPATCH(gxc::user_contract, (login)(connect)(setnick)(payram4nick))
